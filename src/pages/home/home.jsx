726b25de34ec80959f963fb25cf2afbf92605709
hb_kim <binikim66@gmail.com>
2022-12-11 20:30:46 +0900

    Feat: header, navbar, homepage 마크업 완
    header, navbar, homepage 레이아웃 마크업 했습니다.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Header } from '../../components/header/Header'
+import { Navbar } from '../../components/navbar/Navbar'
+import { HomeWrap, HomeLogoImg, SearchBtn, HomeTxt } from './homestyle';
 
 const Home = () => {
   return (
-    <div>home페이지 입니다</div>
+    <HomeWrap>
+      <Header />
+      <HomeLogoImg/>
+      <HomeTxt>딱 맞는 취미 메이트를 찾고 싶다면?</HomeTxt>
+      <SearchBtn>검색하기</SearchBtn>
+      <Navbar />
+    </HomeWrap>
   )
-}
+};
 
-export default Home
+export default Home;
---
+++ src/pages/home/homestyle.jsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components'
+
+export const HomeWrap = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
+export const HomeLogoImg = styled.img`
+
+    margin-top: 48px;
+    width: 100%;
+    height:420px;
+    background-color: pink;
+
+`
+
+export const SearchBtn = styled.button`
+
+    margin-top: 17px;
+    width: 120px;
+    height:44px;
+    background-color: #85CE2D;
+    border-radius: 44px;
+    color: white;
+
+`
+export const HomeTxt = styled.p`
+
+    margin-top: 41px;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 14px;
+
+`
+// 게시글 있을 시
+
+export const HomePostWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+
+`
+
+export const HomeBannerImg = styled.img`
+
+    margin-top: 48px;
+    width: 100%;
+    height:196.5px;
+    background-color: pink;
+
+`
+
+export const Heading = styled.h2`
+
+    margin-top: 58.5px;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 14px;
+    color: #000000;
+
+`
+
+export const HomePostCont = styled.ul`
+
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 20px;
+    gap: 10px;
+
+`
+
+export const HomePostImg = styled.img`
+
+    width: 180px;
+    height: 150px;
+    background-color: pink;
+
+`
+
+export const HomePostTitle = styled.h3`
+
+    margin-top: 9px;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 14px;
+
+`
