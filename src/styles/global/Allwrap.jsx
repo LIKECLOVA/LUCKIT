f7d1b65b73287f3d243053f249e09a371e93934d
seongjin <kssj9056@gmail.com>
2022-12-12 16:33:07 +0900

    Style: Allwrap 높이 넓이 값 수정
    Allwap 높이 넓이 값을 수정했습니다.

--- src/styles/global/Allwrap.jsx
+++ src/styles/global/Allwrap.jsx
@@ -6,10 +6,10 @@ const Allwrap = styled.div`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  width: 100%;
-  height: 100%;
+  width:100%;
+  height:100%;
   max-width: 600px;
-  max-height: 1200px;
+  max-height: 1180px;
   overflow: hidden;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   background-color: #F3FBE9;  
