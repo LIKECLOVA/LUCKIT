2cb9f566e5e4dcd2d6f11952272e2799e46b04b1
seongjin <kssj9056@gmail.com>
2022-12-12 20:14:46 +0900

    Feat: 회원가입 프로필 설정 마크업 완료

--- src/components/join/joinprofilestyle.jsx
+++ src/components/join/joinprofilestyle.jsx
@@ -1,18 +1,60 @@
 import styled from 'styled-components';
 
 export const JoinProfileWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height:100%;
+  padding:25px;
+  h2{
+    margin-top:50px;
+    font-size:40px;
+  }
+  p:nth-of-type(1){
+    font-size:20px;
+    margin-top:14px;
+  }
+  p:nth-of-type(2){
+    font-size:14px;
+    margin-top:14px;
+  }
+  .profileMidWrap{
+    margin-top: 150px;
+    display:flex;
+    img{
+      width: 200px;
+      height: 200px;
+      object-fit:cover;
+    }
+    div{
+      display:flex;
+      flex-wrap: wrap;
+      justify-content: space-evenly;
+      align-items: center;
+    }
+  }
+  .buttonFlexBox{
+    display:flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
 `;
 
 export const ProfileLabel = styled.label`
   display: block;
-  font-size: 12px;
+  .profileMidWrap + & {
+    margin-top: 50px;
+  }
 `;
 
 export const ProfileInput = styled.input`
   display: block;
+  width: 220px;
+  height: 70px;
 `;
+
+export const ProfileTextarea = styled.textarea`
+  width:100%;
+  height:195px;
+  padding:15px;
+  background-color:#F1F1F1;
+  border-radius: 20px;
+  border:none;
+  margin-top:30px;
+`
