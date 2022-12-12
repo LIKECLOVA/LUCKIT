f58d92befa2c17e5ed3d9901723efb2fb3d6a962
bakhyeyeong <owodqufsks@naver.com>
2022-12-13 01:42:42 +0900

    Feat: sns피드 마크업
    sns피드 마크업과 mainsnspost 컴포넌트 작성

---
+++ src/components/mainpost/mainMarketPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mainMarketPost = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default mainMarketPost
--- src/pages/sns/snsPage.jsx
+++ src/pages/sns/snsPage.jsx
@@ -1,6 +1,53 @@
+import React from 'react';
+// import ScrollMenu from "react-horizontal-scrolling-menu";
+import MainSnsPost from '../../components/mainpost/mainSnsPost'
+import { SnsPageArt, SnsPageSec, MainPostArea } from './snsstyle';
+import { Header } from '../../components/header/header';
+import { NavBar } from '../../components/navbar/navBar';
+
+
 export const SnsPage = () => {
+  
+  
   return (
     <>
+    <Header />
+    <SnsPageArt>
+      <ul>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+        <li>
+          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
+        </li>
+      </ul>
+    </SnsPageArt>
+    <SnsPageSec>
+      <h1>럭킷들의 새로운 소식을 확인해보세요!</h1>
+      <MainPostArea>
+      <MainSnsPost/>
+      <MainSnsPost/>
+      </MainPostArea>
+    </SnsPageSec>
+    <NavBar />
     </>
   )
 }
