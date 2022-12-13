fab4b10f4e7d5d6860c14f543db69db8acfcb6da
seongjin <kssj9056@gmail.com>
2022-12-13 11:06:05 +0900

    Feat: myFollow페이지 yourFollow페이지 follow 컴포넌트 마크업 완료
    내 프로필 페이지. 상대방 프로필 페이지에 있는 팔로워 팔로잉을 누르면 이동하는 페이지 마크업 완료했습니다.
    유저 정보가 뜨는 팔로우 컴포넌트도 마크업 완료했습니다.

--- src/components/follow/follow.jsx
+++ src/components/follow/follow.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { FollowInfoWrap, FollowLi } from './followstyle';
 
-export const follow = () => {
+export const Follow = () => {
   return (
-    <div>follow</div>
-  )
-}
+   
+    <FollowLi>
+      <FollowInfoWrap>
+        <img src='https://mandarin.api.weniv.co.kr/Ellipse.png' alt='프로필사진'/>
+        <div>
+          <p>닉네임</p>
+          <p>자기소개@@@@@@@</p>
+        </div>
+      </FollowInfoWrap>
+      <button>버튼자리입니다</button>
+    </FollowLi>
+   
+  );
+};
--- src/pages/follow/myFollow.jsx
+++ src/pages/follow/myFollow.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FollowPageUl, FollowPageWrap } from './followstyle';
+import { Header } from '../../components/header/header';
+import { Follow } from '../../components/follow/follow';
 
 export const MyFollow = () => {
+/* 내 프로필 페이지의 팔로잉 팔로워 누르면 이동하는 페이지 */
+
   return (
-    <div>follow</div>
-  )
-}
+    <>
+      <Header />
+      <FollowPageWrap>
+        <FollowPageUl>
+          <Follow/>
+          <Follow/>
+          <Follow/>
+        </FollowPageUl>
+      </FollowPageWrap>
+    </>
+  );
+};
