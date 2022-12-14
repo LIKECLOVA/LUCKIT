fe52088e70a50971de280ecf702204963071e377
hb_kim <binikim66@gmail.com>
2022-12-14 13:48:59 +0900

    Refactor: 네비바 마크업 수정
    네비바 마크업 수정, hover, active 시 아이콘과 색상 변경

--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -1,25 +1,68 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { NavWrap, NavList, NavListItem } from './navbarstyle'
+import { NavWrap, StyleNavLink } from './navbarstyle'
+import homeIcon from '../../assets/home-disabled.png'
+import homeIconFill from '../../assets/home-클로바.png'
+import snsIcon from '../../assets/document.png'
+import snsIconFill from '../../assets/document-disabled.png'
+import chatIcon from '../../assets/chat-disabled.png'
+import chatIconFill from '../../assets/chat.png'
+import profileIcon from '../../assets/profile-disabled.png'
+import profileIconFill from '../../assets/profile.png'
+
 
 export const NavBar = () => {
   return (
     <NavWrap>
-	    <NavList>
-        <NavListItem>
-           <NavLink to='/'>홈</NavLink>
-	      </NavListItem>
-        <NavListItem>
-           <NavLink to='/snspage'>SNS</NavLink>
-	      </NavListItem>
-        <NavListItem>
-	         <NavLink to='/chatpage'>채팅</NavLink>
-        </NavListItem>
-        <NavListItem>
-	        <NavLink to='/myprofile'>프로필</NavLink>
-        </NavListItem>
-      </NavList>
+	    <ul>
+        <li>
+          <StyleNavLink 
+            to={'/'}
+            icon={homeIcon}
+            hoverIcon={homeIconFill}
+            className={({isActive}) => {
+              return (
+                isActive ? 'active' : null  
+              )
+              }}>홈
+            </StyleNavLink>
+	      </li>
+        <li>
+          <StyleNavLink 
+            to={'/snspage'} 
+            icon={snsIcon}
+            hoverIcon={snsIconFill}
+            className={({isActive}) => {
+              return (
+                isActive ? 'active' : null  
+              )
+              }}>SNS
+          </StyleNavLink>
+	      </li>
+        <li>
+          <StyleNavLink 
+            to={'/chatpage'} 
+            icon={chatIcon}
+            hoverIcon={chatIconFill}
+            className={({isActive}) => {
+              return (
+                isActive ? 'active' : null  
+              )
+              }}>채팅
+          </StyleNavLink>
+	      </li>
+        <li>
+          <StyleNavLink 
+            to={'/myprofile'} 
+            icon={profileIcon}
+            hoverIcon={profileIconFill}
+            className={({isActive}) => {
+              return (
+                isActive ? 'active' : null  
+              )
+              }}>프로필
+          </StyleNavLink>
+	      </li>
+      </ul>
     </NavWrap>
-
   )
 }
