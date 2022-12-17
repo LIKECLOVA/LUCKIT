8f5e8a42b2720c90650012bef0a3b748eca46af9
hb_kim <binikim66@gmail.com>
2022-12-17 19:39:05 +0900

    Feat: 마이프로필 페이지 일반 게시글 fetch사용해 렌더링 구현
    마이 프로필 페이지 하단 fetch 사용해 일반 게시글 렌더링 구현

--- src/pages/profile/myProfile.jsx
+++ src/pages/profile/myProfile.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { ProfileAndChatHeader } from '../../components/header/header';
 import { MarketPreviewPost } from '../../components/market-preview-post/marketPreviewPost';
 import { ProfileBox } from '../../components/profile-box/profileBox';
 import { MyProfileWrap, SnsPostBtn } from './myprofilestyle';
 import { NavBar } from '../../components/navbar/navBar'
-import MainSnsPost from '../../components/mainpost/mainSnsPost';
+import {MainSnsPostWhap, IconWrap, SnsIdWhap, SnsPostBox} from '../../components/mainpost/mainpoststyle'
+import { MoreBtn } from "../../components/button/iconBtn";
 
 export const MyProfile = () => {
+  const [snsPostsData, setSnsPostsData] = useState();
+
+  const accountname = 'clover2'
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOWMyMDY3MTdhZTY2NjU4MWM2NGNhNCIsImV4cCI6MTY3NjM2NzU3MSwiaWF0IjoxNjcxMTgzNTcxfQ.DwRg_udzt-BG1TCQ43G2gHRNy72fSux7QaSjnZrdt5w'
+
+  useEffect(() => {
+
+    snsPostList();
+    
+  }, [])
+
+  const snsPostList = async () => {
+    await fetch(`https://mandarin.api.weniv.co.kr/post/${accountname}/userpost/?limit=5`, {
+      method: 'GET',
+      headers: {
+        "Authorization" : `Bearer ${token}`,
+        "Content-type" : "application/json"
+      },
+    }).then(res => {
+      return res.json()
+    }).then(data => {
+      setSnsPostsData(data.post)
+    })
+  }
+
   return (
     <>
       <ProfileAndChatHeader />
@@ -19,15 +46,38 @@ export const MyProfile = () => {
           <button>Thum</button>
         </SnsPostBtn>
         <ul>
-          <li><MainSnsPost /></li>
-          <li><MainSnsPost /></li>
-          <li><MainSnsPost /></li>
-          <li><MainSnsPost /></li>
-          <li><MainSnsPost /></li>
+          {snsPostsData && snsPostsData.map((post) => {
+            return (
+              <MainSnsPostWhap key={post.id}>
+                <NavLink to='/yourprofile'>
+                  <img src={post.author.image} alt="프로필이미지" />
+                </NavLink>
+                <SnsPostBox>
+                <NavLink to='/yourprofile'>
+                <SnsIdWhap>
+                  <strong>{post.author.username}</strong>
+                  <p>@ {post.author.accountname}</p>
+                </SnsIdWhap>
+                </NavLink>
+                <NavLink to='/snsPost'>
+                  <p>{post.content}</p>
+                  <img src={post.image} alt="게시글이미지" />
+                </NavLink>
+                <IconWrap>
+                <button>좋아요버튼</button>
+                <NavLink to='/snsPost'>
+                  <button>댓글버튼</button>
+                </NavLink>
+                </IconWrap>
+                </SnsPostBox>
+                <MoreBtn/>
+            </MainSnsPostWhap>
+            )
+          })}
         </ul>
       </section>
       </MyProfileWrap>
-      <NavBar />
+      <NavBar />     
     </>
   );
 };
