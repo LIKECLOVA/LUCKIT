f3c120383b9443f26a614e381cd90bfb13569340
bakhyeyeong <owodqufsks@naver.com>
2022-12-19 02:48:01 +0900

    Feat: sns피드 게시물조회 구현
    sns피드 페이지 게시물 조회기능 구현

--- src/pages/sns/snsPage.jsx
+++ src/pages/sns/snsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect , useState} from 'react';
 // import ScrollMenu from "react-horizontal-scrolling-menu";
 import MainSnsPost from '../../components/mainpost/mainSnsPost'
 import { SnsPageArt, SnsPageSec, MainPostArea } from './snsstyle';
@@ -14,42 +14,62 @@ export const SnsPage = () => {
   //       setModalOpen(!modalOpen);
   //   };
   
+  const [list ,setList] = useState([]);
+  // const [followList,setFollowList] = useState([]);
+  const URL = `https://mandarin.api.weniv.co.kr`;
+  const FEED_PATH = `/post/feed`;
+  // const STORY_PATH=`/profile/:accountname/following`;
+  const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTA5MzIwMTdhZTY2NjU4MWMwMzNlNyIsImV4cCI6MTY3NjQ0NDc2OSwiaWF0IjoxNjcxMjYwNzY5fQ.PcmkXNY7JTV8PlIYVh9XOCbYhiD789NfFYXrjOQ6_ik';
+  
+  async function fetchFeedPostData(){
+    await fetch(URL+FEED_PATH, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    })
+    .then((data) => data.json())
+    .then((data) =>  {
+      setList(data.posts)
+      // console.log(data.posts)
+    });
+  }
+  // async function fetchUserStoryData() {
+    
+  //   await fetch( URL+STORY_PATH, {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //       'Content-type': 'application/json',
+  //     },
+  //   })
+  //     .then((data) => data.json())
+  //     .then((data) => console.log([...data]))
+  //     /* setFollowList([...data]));*/
+  // }
+  useEffect( ()=>{
+    fetchFeedPostData();
+    // fetchUserStoryData();
+  },[])
+
   return (
     <>
     <FeedPageHeader />
     <SnsPageArt>
       <ul>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
-        <li>
-          <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="스토리이미지1" />
-        </li>
+      {/* {followList.map((story)=> {
+           return <li><img src={story.image} /></li>
+        })} */}
       </ul>
     </SnsPageArt>
     <SnsPageSec>
       <h1>럭킷들의 새로운 소식을 확인해보세요!</h1>
       <MainPostArea>
-      <MainSnsPost/>
-      <MainSnsPost/>
+        {list.map((value)=> {
+           return < MainSnsPost data={value} key={value._id} />
+        })}
       </MainPostArea>
     </SnsPageSec>
     <NavBar />
