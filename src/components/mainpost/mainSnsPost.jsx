d965a87e45467a5e4b41709101060e262168ae97
bakhyeyeong <owodqufsks@naver.com>
2022-12-19 02:48:01 +0900

    Feat: sns피드 게시물조회 구현
    sns피드 페이지 게시물 조회기능 구현

--- src/components/mainpost/mainSnsPost.jsx
+++ src/components/mainpost/mainSnsPost.jsx
@@ -3,22 +3,22 @@ import { NavLink } from 'react-router-dom'
 import {MainSnsPostWhap,IconWrap, SnsIdWhap, SnsPostBox} from './mainpoststyle'
 import { MoreBtn } from "../button/iconBtn";
 
-const MainSnsPost = () => {
+const MainSnsPost = ({data}) => {
   return (
     <MainSnsPostWhap>
        <NavLink to='/yourprofile'>
-      <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="프로필이미지" />
+      <img src={data.author.image} alt="프로필이미지" />
       </NavLink>
       <SnsPostBox>
       <NavLink to='/yourprofile'>
       <SnsIdWhap>
-      <strong>몰랑몰랑</strong>
-      <p>@molang</p>
+      <strong>{data.author.username}</strong>
+      <p>@{data.author.accountname}</p>
       </SnsIdWhap>
       </NavLink>
       <NavLink to='/snsPost'>
-      <p>몰랑이는 너무 귀여운 것 같아용!</p>
-      <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20160622_226%2Fsang7145_1466595386985CQdGk_JPEG%2Fg.jpg&type=sc960_832" alt="게시글이미지" />
+      <p>{data.content}</p>
+      <img src={data.image} alt="게시글이미지" />
       </NavLink>
       <IconWrap>
       <button>좋아요버튼</button>
