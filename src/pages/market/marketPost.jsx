a7ebf4dce4a8b73c5d07efb711784b1707d4c11f
yuzomi <im.the.best@kakao.com>
2022-12-26 23:40:56 +0900

    Fix: 상품 수정 기능 구현 수정

--- src/pages/market/marketPost.jsx
+++ src/pages/market/marketPost.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ProfileAndChatHeader } from '../../components/header/header';
 import MarketPostBox from '../../components/mainpost/marketPostBox';
@@ -12,46 +12,48 @@ export function MarketPost() {
   const { id } = useParams();
   const token = localStorage.getItem('Access Token');
 
-useEffect(() => {
+  useEffect(() => {
     axios({
-        method: 'get',
-        url: `https://mandarin.api.weniv.co.kr/product/${id}/?limit=20`,
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-type': 'application/json',
-        },
-    }).then((res) => {
-            setMarketPostsData(res.data.product);
-        })
-        .then((error) => {
-            console.log(error);
-        });
-}, [])
+      method: 'get',
+      url: `https://mandarin.api.weniv.co.kr/product/${id}/?limit=20`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setMarketPostsData(res.data.product);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
-        <ProfileAndChatHeader />
-        <MarketPostWrap>
+      <ProfileAndChatHeader />
+      <MarketPostWrap>
         <h2>마켓 게시글 페이지</h2>
         <MarketPostTitle>
-            <strong>{marketPostsData && marketPostsData[0].author.username}</strong> 님이 찾는 럭킷 메이트<br />
-            네잎클로버를 눌러 매칭을 신청해 보세요!✨
+          <strong>{marketPostsData && marketPostsData[0].author.username}</strong> 님이 찾는 럭킷 메이트
+          <br />
+          네잎클로버를 눌러 매칭을 신청해 보세요!✨
         </MarketPostTitle>
         <main>
-            <ListWrap>
+          <ListWrap>
             {marketPostsData &&
-                marketPostsData.map((data, index) => {
+              marketPostsData.map((data, index) => {
                 return (
-                <ListItem key={Math.random()}>
+                  <ListItem key={Math.random()}>
                     <MarketPostBox data={data} index={index} id={id} />
-                    <MarketPostMoreBtn productId={data.id}/>
-                </ListItem>
-                )
-            })}
-            </ListWrap>
+                    <MarketPostMoreBtn productId={data.id} />
+                  </ListItem>
+                );
+              })}
+          </ListWrap>
         </main>
-        </MarketPostWrap>
-        <NavBar />
+      </MarketPostWrap>
+      <NavBar />
     </>
-  )
+  );
 }
