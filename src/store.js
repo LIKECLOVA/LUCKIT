4426f0bc130f5aa55240a0b6fe8bab92d42c7900
seongjin <kssj9056@gmail.com>
2022-12-30 16:32:50 +0900

    Refactor: 프로필 페이지 상품목록 불러오기 리팩토링
    리덕스 thunk로 상품목록을 관리하려고 리팩토링 하였습니다.

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userInfoSlice } from "./reducers/getUserInfoSlice";
 import { productListSlice } from "./reducers/getProductListSlice";
 import { followInfoSlice } from "./reducers/getFollowSlice";
+import { snsPostSlice } from "./reducers/getSnsPostSlice";
 
 export const store =  configureStore({
     reducer:{
         userInfoSlice : userInfoSlice.reducer,
         productListSlice : productListSlice.reducer,
-        followInfoSlice: followInfoSlice.reducer
+        followInfoSlice: followInfoSlice.reducer,
+        snsPostSlice : snsPostSlice.reducer
     }
 })
