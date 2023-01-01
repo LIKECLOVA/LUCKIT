fafd61112279b1152bd72af962b90c809bd8eb32
yuzomi <im.the.best@kakao.com>
2023-01-02 00:01:06 +0900

    Design: CSS 마진 수정

--- src/components/mainpost/mainSnsPost.jsx
+++ src/components/mainpost/mainSnsPost.jsx
@@ -19,52 +19,54 @@ const onErrorImg = (e) => {
   e.target.src = DefaultUserImg;
 };
 
-
-const MainSnsPost = ({data}) => {
+const MainSnsPost = ({ data }) => {
   const userToken = localStorage.getItem('Access Token');
 
   return (
     <MainSnsPostWhap>
-       <NavLink to={`/profile/${data.author.accountname}`}>
-      <UserProfileImg src={data.author.image} onError={onErrorImg} />
+      <NavLink to={`/profile/${data.author.accountname}`}>
+        <UserProfileImg src={data.author.image} onError={onErrorImg} />
       </NavLink>
       <SnsPostBox>
-      <NavLink to={`/profile/${data.author.accountname}`}>
-      <SnsIdWhap>
-      <strong>{data.author.username}</strong>
-      <p>@{`${data.author.accountname}`}</p>
-      </SnsIdWhap>
-      </NavLink>
-      <NavLink to={`/snspost/${data.id}`}>
-      <SnsPostContent>{data.content}</SnsPostContent>
-      <div className='snsImgList'>
-                {data.image ? data.image.split(',').map((src, index)=> {
-                    return (
-                    <ImgListDiv key={index}>
-                        <img src={src} onError = {e =>{e.target.style.display = 'none'}} alt="게시글이미지" />
-                    </ImgListDiv>
-                    )
-                })
-                : 
-                <></>}
-                </div>
-      </NavLink>
-      <IconWrap>
-      <HeartBtn
-          userToken={userToken}
-          hearted={data.hearted}
-          postId={data.id}
-          heartCount={data.heartCount}
-        />
-      <NavLink to={`/snspost/${data.id}`}>
-      <CommentBtnWrap>
-        <img src={CommentIcon} alt="" style={{ marginRight: '6px' }} />
-        <span>{data.comments.length}</span>
-      </CommentBtnWrap>
-      </NavLink>
-      </IconWrap>
+        <NavLink to={`/profile/${data.author.accountname}`}>
+          <SnsIdWhap>
+            <strong>{data.author.username}</strong>
+            <p>@{`${data.author.accountname}`}</p>
+          </SnsIdWhap>
+        </NavLink>
+        <NavLink to={`/snspost/${data.id}`}>
+          <SnsPostContent>{data.content}</SnsPostContent>
+          <div className='snsImgList'>
+            {data.image ? (
+              data.image.split(',').map((src, index) => {
+                return (
+                  <ImgListDiv key={index}>
+                    <img
+                      src={src}
+                      onError={(e) => {
+                        e.target.style.display = 'none';
+                      }}
+                      alt='게시글이미지'
+                    />
+                  </ImgListDiv>
+                );
+              })
+            ) : (
+              <></>
+            )}
+          </div>
+        </NavLink>
+        <IconWrap>
+          <HeartBtn userToken={userToken} hearted={data.hearted} postId={data.id} heartCount={data.heartCount} />
+          <NavLink to={`/snspost/${data.id}`}>
+            <CommentBtnWrap>
+              <img src={CommentIcon} alt='' style={{ marginRight: '3px' }} />
+              <span>{data.comments.length}</span>
+            </CommentBtnWrap>
+          </NavLink>
+        </IconWrap>
       </SnsPostBox>
-      <MoreBtn postId={data.id} accountName={data.author.accountname} postContent={data.content} postImg={data.image}/>
+      <MoreBtn postId={data.id} accountName={data.author.accountname} postContent={data.content} postImg={data.image} />
     </MainSnsPostWhap>
   );
 };
