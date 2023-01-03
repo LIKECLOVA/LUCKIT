514c92ca1841c2ad83586b583c99a6c2c93a13b8
bakhyeyeong <owodqufsks@naver.com>
2023-01-04 00:14:56 +0900

    Fix:sns업로드페이지 css 수정
    sns업로드페이지 css 수정

--- src/pages/sns/snsstyle.jsx
+++ src/pages/sns/snsstyle.jsx
@@ -4,49 +4,19 @@ import deleteBtn from '../../assets/icon/x.png';
 import { palette } from '../../styles/global/globalColor';
 
 /* SnsUpload 페이지 */
-const SnsUploadArt = styled.article`
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #ddd;
-  box-sizing: border-box;
-  width: 100%;
-  height: 48px;
-  padding: 8px 11px;
-  margin: 0px auto;
-  position: fixed;
-  top: 0px;
-  background: #fff;
-  display: flex;
-  z-index: 10;
-  button {
-    cursor: pointer;
-    :last-child {
-      margin-left: 80%;
-    }
-  }
-`;
 
-export { SnsUploadArt };
-
-const SnsUploadSec = styled.section`
-  top: 4%;
+export const SnsUploadSec = styled.section`
   padding: 30px 15px;
   justify-content: center;
   width: 100%;
   height: 90%;
-  & > input {
-  }
 `;
 
-export { SnsUploadSec };
-
-const SnsTextLable = styled.label`
+export const SnsTextLable = styled.label`
   display: none;
 `;
 
-export { SnsTextLable };
-
-const SnsTextInput = styled.textarea`
+export const SnsTextInput = styled.textarea`
   width: 100%;
   height: 45%;
   padding: 35px 0;
@@ -57,21 +27,15 @@ const SnsTextInput = styled.textarea`
   resize: none;
 `;
 
-export { SnsTextInput };
-
-const SnsUploadImg = styled.div`
+export const SnsUploadImg = styled.div`
   box-sizing: border-box;
   width: 100%;
   height: 300px;
   bottom: 130px;
-  left: 0;
-  right: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
-
-export { SnsUploadImg };
 
 export const FileUploader = styled.label`
   background: url(${imgBtn});
@@ -83,7 +47,6 @@ export const FileUploader = styled.label`
   height: 45px;
   bottom: 20px;
   right: 20px;
-  z-index: 90;
 `;
 
 export const FileInput = styled.input`
@@ -95,11 +58,8 @@ export const SingleImg = styled.img`
   height: 200px;
   border-radius: 10px;
   object-fit: cover;
-  @media screen and (max-width: 380px) {
-    width: 150px;
-    height: 90px;
-  }
 `;
+
 export const DeleteBtn = styled.button`
   cursor: pointer;
   position: absolute;
@@ -109,21 +69,18 @@ export const DeleteBtn = styled.button`
   background-repeat: no-repeat;
   background-size: cover;
 `;
+
 export const Img = styled.img`
   object-fit: cover;
   position: relative;
-  margin: 5px 5px 5px 5px;
+  margin: 5px;
   width: 200px;
   height: 130px;
   border-radius: 10px;
-  @media screen and (max-width: 380px) {
-    width: 150px;
-    height: 90px;
-  }
 `;
 
 /* sns 페이지 */
-const SnsPageArt = styled.article`
+export const SnsPageArt = styled.article`
   align-items: center;
   box-sizing: border-box;
   width: 95%;
@@ -158,9 +115,7 @@ const SnsPageArt = styled.article`
   }
 `;
 
-export { SnsPageArt };
-
-const SnsStoryImg = styled.img`
+export const SnsStoryImg = styled.img`
   box-sizing: border-box;
   object-fit: cover;
   border: 3px solid ${palette.초록초록};
@@ -169,9 +124,7 @@ const SnsStoryImg = styled.img`
   height: 60px;
 `;
 
-export { SnsStoryImg };
-
-const SnsPageSec = styled.section`
+export const SnsPageSec = styled.section`
   align-items: center;
   box-sizing: border-box;
   width: 100%;
@@ -209,9 +162,7 @@ const SnsPageSec = styled.section`
   }
 `;
 
-export { SnsPageSec };
-
-const MainPostArea = styled.div`
+export const MainPostArea = styled.div`
   align-items: left;
   box-sizing: border-box;
   width: 100%;
@@ -224,5 +175,3 @@ const MainPostArea = styled.div`
   flex-direction: column;
   margin-bottom: 30px;
 `;
-
-export { MainPostArea };
